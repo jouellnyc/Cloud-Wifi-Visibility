cb8c410a62b3844ec5f6d49619ff823dc6e0946c
jouellnyc <jouellnyc@gmail.com>
2021-10-17 15:16:06 -0400

    Jinja file template

--- isc-dhcp/gen_dhcpd_conf.py
+++ isc-dhcp/gen_dhcpd_conf.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+""" gen_dhcpd_conf.py - Create an entry for MACs not masked by the vendor's """
+""" extender and one that is for each network on the LAN using hosts2mac as """
+""" primary source of information - (Name|MAC|last IP octect)               """
+""" Each final entry contains MAC,IP and Name (see template)                """
+
 from jinja2 import Template
 
 """ Set your Vendor Prefix """
@@ -11,22 +16,20 @@ EX_MAC = "22:28:f3"
 """ Set your Desired /24 Nets """
 networks = ["192.168.0.0", "10.0.0.0"]
 
-DHCPD_FILE = "dhcpd.hosts.stub.tmp"
-HOSTS_FILE = "hosts.tmp"
-
-template = """host {{ hostname }} {
-    hardware ethernet {{ mac }};
-    fixed-address {{ ip }};
-}"""
-
+DHCPD_STUB     = "dhcpd.hosts.stub.tmp"
+HOSTS_FILE     = "hosts.tmp"
+DHCPD_TEMPLATE = "dhcpd.template"
 
 def set_jinja(data, fh):
     fh.write(j2_template.render(data) + "\n")
 
-
 try:
 
-    dh = open(DHCPD_FILE, "w")
+    #Open Template and be done with it
+    with open(DHCPD_TEMPLATE,'r') as dt:
+        j2_template = Template(dt.read())
+
+    dh = open(DHCPD_STUB, "w")
     eh = open(HOSTS_FILE, "w")
 
     for line in open("hosts2mac.txt", "r"):
@@ -46,7 +49,6 @@ try:
             sn_pre = one_network.split(".")[0]
             hostname = f"{name}-{sn_pre}"
 
-            j2_template = Template(template)
 
             """ Normal Mac """
             data = {"mac": mac, "ip": ip, "hostname": hostname}
