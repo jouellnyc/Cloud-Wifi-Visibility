68c2fa14469610ced4a0a13d7cd827bd31936c78
jouellnyc <jouellnyc@gmail.com>
2021-12-25 13:31:07 -0500

    +Update for /etc/hosts -- both networks

--- isc-dhcp/gen_dhcpd_conf.py
+++ isc-dhcp/gen_dhcpd_conf.py
@@ -1,77 +1,52 @@
 #!/usr/bin/python3
 
-""" gen_dhcpd_conf.py - Create an entry for MACs not masked by the vendor's """
-""" extender and one that is for each network on the LAN using hosts2mac as """
-""" primary source of information - (Name|MAC|last IP octect)               """
-""" Each final entry contains MAC,IP and Name (see template)                """
-
-import sys
-from jinja2 import Template
-
-""" Set your Vendor Prefix """
+""" Set your Vendor Prefix """ 
 EX_VENDOR = "TP"
 
-""" Set your Vendor Mac Prefix """
-EX_MAC = "22:28:f3"
+""" Set your Vendor Mac Prefix """ 
+EX_MAC ="22:28:f3"
 
-""" Set your Desired /24 Nets """
-networks = ["192.168.0.0", "10.0.0.0"]
+""" Set your Desired /24 Nets """ 
+networks = ['192.168.0.0', '10.0.0.0']
 
-DHCPD_STUB     = "dhcpd.hosts.stub.tmp"
-HOSTS_FILE     = "hosts.tmp"
-DHCPD_TEMPLATE = "dhcpd.template"
-MAC_DB         = "hosts2mac.txt"
-
-def set_jinja(data, fh):
-    fh.write(j2_template.render(data) + "\n")
+DHCPD_FILE='dhcpd.hosts.stub.tmp'
+HOSTS_FILE='hosts.tmp'
 
 try:
 
-    #Open Template and be done with it
-    with open(DHCPD_TEMPLATE,'r') as dt:
-        j2_template = Template(dt.read())
+    fh = open(DHCPD_FILE,'w')
+    eh = open(HOSTS_FILE,'w')
 
-    dh = open(DHCPD_STUB, "w")
-    eh = open(HOSTS_FILE, "w")
+    for line in open('hosts2mac.txt','r'):
 
-    for line in open(MAC_DB, "r"):
-
-        if line.startswith("#"):
+        if line.startswith('#'):
             continue
 
         for one_network in networks:
 
-            mac, number, name = line.split(",")
+            mac,number,name = line.split(',')
             name = name.strip()
 
-            quads = one_network.split(".")
-            quads[-1] = number
-            ip = ".".join(quads)
+            quads = one_network.split('.')
+            quads[-1] =  number
+            ip  = '.'.join(quads) 
+            sn_pre = one_network.split('.')[0]
 
-            sn_pre = one_network.split(".")[0]
-            hostname = f"{name}-{sn_pre}"
+            fh.write(f"host {name}-{sn_pre} {{\n")
+            fh.write(f"   hardware ethernet {mac};\n")
+            fh.write(f"   fixed-address {ip};\n")
+            fh.write(f"}}\n")
 
-
-            """ Normal Mac """
-            data = {"mac": mac, "ip": ip, "hostname": hostname}
-            set_jinja(data, dh)
-
-            """ Vendor Prefix Mac """
-            data = {
-                "mac": EX_MAC + mac[8:],
-                "ip": ip,
-                "hostname": f"{hostname}-{EX_VENDOR}",
-            }
-            set_jinja(data, dh)
+            mac = mac[8:]
+            mac = EX_MAC +  mac
+            fh.write(f"host {name}-{sn_pre}-{EX_VENDOR} {{\n")
+            fh.write(f"   hardware ethernet {mac};\n")
+            fh.write(f"   fixed-address {ip};\n")
+            fh.write(f"}}\n")
 
         eh.write(f"{ip}   {name}\n")
-
 except OSError as e:
     print(e)
-except ValueError as e:
-    if 'not enough values to unpack' in str(e):
-        print(f"Error: {e}, Did you format {MAC_DB} correctly?") 
-        sys.exit(1)
 finally:
-    dh.close()
+    fh.close()
     eh.close()
